d0dbccc80a9f03aa1fc105f8c268abe3e2da9734
tuannh982 <tuannh982@gmail.com>
2024-07-03 01:14:30 +0700

    add binary files

---
+++ bin/new-module.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -u
+set -eo pipefail
+
+MODULE_NAME="$1"
+mkdir -p "$MODULE_NAME"
+mkdir -p "$MODULE_NAME/src/main/scala"
+touch "$MODULE_NAME/src/main/scala/Main.scala"
+echo 'name := "'"$MODULE_NAME"'"' > "$MODULE_NAME/build.sbt"
